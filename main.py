90337d31d8ea96dbaf56505d4d88918b1d5206da
LeonAdeoye <leon.adeoye@gmail.com>
2021-05-24 22:25:33 +0800

    read insurance csv

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-# coding=utf-8
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
 
 
-# Press the green button in the gutter to run the script.
+def read_data():
+    print(f'Reading CSV file: insurance.csv into dataframe with Panda {pd.__version__}')
+    # read a CSV file with a comma as a delimiter, assume no header, and skip the first column
+    insurance_dataFrame = pd.read_csv("../data/insurance.csv", sep=',')
+    print(f"Head after read:\n{insurance_dataFrame.head()}")
+    print(f"Shape:\n{insurance_dataFrame.shape}")
+    insurance_dataFrame["charges"].hist(bins=10)
+    plt.show()
+
+
+def normalize(df):
+    df_scaled = df.copy()
+    for column in df_scaled.columns:
+        if column == 1:
+            df_scaled[column] = df_scaled[column]
+        else:
+            df_scaled[column] = (df_scaled[column] - df_scaled[column].min()) / (
+                        df_scaled[column].max() - df_scaled[column].min())
+    return df_scaled
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    read_data()
